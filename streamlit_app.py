3fa4b5cc799839a2c28656dd1601aed5c5a35af9
matthewmintz08 <69319245+matthewmintz08@users.noreply.github.com>
2023-09-20 17:11:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,8 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
+import ezdxf
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    # To read file as bytes:
-
-    # To convert to a string based IO:
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
+    doc = ezdxf.readfile(uploaded_file)
